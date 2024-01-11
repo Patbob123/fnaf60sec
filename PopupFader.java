456f7278a9ca28573a99f16c4711f871ce8209cb
Mangofish369 <340868470@gapps.yrdsb.ca>
2024-01-11 11:10:02 -0500

    tile

---
+++ PopupFader.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PopupFader here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PopupFader extends Effect
+{
+    /**
+     * Act - do whatever the PopupFader wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ Tile.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Tile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tile extends Actor
+{
+    private int width;
+    private int height;
+    
+    public Tile(int width, int height){
+        this.width = width;
+        this.height = height;
+        GreenfootImage temp = new GreenfootImage(width,height);
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
