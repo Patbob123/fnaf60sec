70e4639f381f3a6b81702e9d822be86d22fa45e9
JenZ-00 <349941245@gapps.yrdsb.ca>
2024-01-18 00:32:50 -0500

    Added the basic camera map buttons

---
+++ CameraMap.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CameraMap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CameraMap extends Actor
+{
+    /**
+     * Display the map of the cameras
+     * Collapsable
+     * Set of buttons
+     * Background image
+     * Once touched the map button, display the bg and add all the other buttons
+     */
+    public void act()
+    {
+        
+    }
+}
--- SchoolWorld.java
+++ SchoolWorld.java
@@ -13,8 +13,16 @@ public abstract class SchoolWorld extends World
     protected HashMap<String, World> worldStorage;
     protected Player fisho;
     protected PressurePlate left, right, up, down;
-    
+
     private Button map;
+    private Button cam1;
+    private Button cam2;
+    private Button cam3;
+    private Button cam4;
+    private Button cam5;
+    private Button cam6;
+    
+    private int numClicks = 0;
     /**
      * Constructor for objects of class SchoolWorlds.
      * 
@@ -22,10 +30,13 @@ public abstract class SchoolWorld extends World
     public SchoolWorld(WorldManager wManager)
     {
         super(1152, 768, 1);
+        //Greenfoot.setWorld(this);
         this.wManager = wManager;
         worldStorage = wManager.getHashMap();
-        map();
+        map = new Button("+", 40);
+        addObject(map, 1057, 36);
     }
+
     public boolean isCreated(String worldName){
         if(wManager.checkIfCreated(worldName) == false){
             wManager.storeWorld(worldName, this);
@@ -33,8 +44,34 @@ public abstract class SchoolWorld extends World
         }
         return true;
     }
-    public void map(){
-        map = new Button("map", 40);
-        addObject(map, 1091, 45);
+
+    public void act(){
+        if (Greenfoot.mousePressed(map)){
+            generateCamMap();
+            numClicks++;
+            if(numClicks % 2 == 0){
+                map.updateMe("+");
+            }else{
+                map.updateMe("-");
+            }
+        }
+    }
+
+    private void generateCamMap(){
+        if (cam1==null){
+            cam1 = new Button("CAM1", 20);
+            cam2 = new Button("CAM2", 20);
+            cam3 = new Button("CAM3", 20); 
+            cam4 = new Button("CAM4", 20);
+            cam5 = new Button("CAM5", 20);
+            cam6 = new Button("CAM6", 20);
+            addObject(cam1, 995, 79);
+            addObject(cam2, 1057, 79);
+            addObject(cam3, 1119, 79);
+
+            addObject(cam4, 995, 114);
+            addObject(cam5, 1057, 114);
+            addObject(cam6, 1119, 114);
+        }
     }
 }
