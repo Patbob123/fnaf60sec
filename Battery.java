8417853a4abd45c9685d2a7df97eee10af7c8ea6
ashmcd <jaidening340@gmail.com>
2024-01-20 01:20:48 -0500

    battery item

---
+++ Battery.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Battery here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Battery extends Item
+{
+    private int power;
+    public Battery(){
+        weight = 1;
+        power = 10;
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
