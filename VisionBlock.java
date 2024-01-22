830b91c9f5e642c9d18835a9f8d5ba89ca89609b
JenZ-00 <349941245@gapps.yrdsb.ca>
2024-01-22 11:22:55 -0500

    visionBlock

---
+++ VisionBlock.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class VisionBlock here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class VisionBlock extends Effect
+{
+    /**
+     * Act - do whatever the VisionBlock wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private int width;
+    private int height;
+    private Color fading;
+    public VisionBlock(int width, int height, int visionTime)
+    {
+        this.width = width;
+        this.height = height;
+        //fading = new Color(34, 24, 59, visionTime);
+        fading = new Color(244, 228, 12, visionTime);
+        
+        GreenfootImage visionBlock = new GreenfootImage(width, height);
+        visionBlock.setColor(fading);
+        visionBlock.fill();
+        setImage(visionBlock);
+    
+    }
+}
